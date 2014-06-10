9580faa7634fdda3f7517cc8eccc0b72de85d032
codeForGxm <guoxiaomin@guoxiaomindeMacBook-Pro.local>
2014-06-10 16:39:11 +0800

    testOne
    测试一下 github 的使用

---
+++ testGitHub/testGitHub/GXMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GXMAppDelegate.h
+//  testGitHub
+//
+//  Created by 郭晓敏 on 14-6-10.
+//  Copyright (c) 2014年 lanou3g.com 蓝鸥科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GXMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testGitHub/testGitHub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testGitHub
+//
+//  Created by 郭晓敏 on 14-6-10.
+//  Copyright (c) 2014年 lanou3g.com 蓝鸥科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GXMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GXMAppDelegate class]));
+    }
+}
